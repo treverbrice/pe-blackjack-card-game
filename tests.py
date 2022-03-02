8d3be96b7825b0b37041bb8a4f403c7fc5d525cb
treverbrice <treverbrice@gmail.com>
2022-03-02 07:12:09 -0800

    added tests.py file to contain all class testing

---
+++ tests.py
@@ -0,0 +1,23 @@
+'''import card
+
+test = card.card('spade', 'a')
+value = test.getValue()
+card = test.toStr()
+
+print(value)
+print(card)'''
+
+'''import deck
+
+my_cards = deck.deck()
+print(my_cards.cards)
+my_cards.shuffle()
+print(my_cards.cards)
+
+half_deck = []
+for i in range(26):
+    one_card = my_cards.deal()
+    half_deck.append(one_card)
+
+print(half_deck)
+print(my_cards.cards)'''
