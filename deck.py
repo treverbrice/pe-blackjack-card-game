99c38380766d9a5b32635bdb1db701f0389d8fc5
treverbrice <treverbrice@gmail.com>
2022-03-04 05:49:03 -0800

    added rest method that is identical to init

--- deck.py
+++ deck.py
@@ -30,3 +30,22 @@ class deck:
     def deal(self):
         top_card = self.cards.pop(0)
         return top_card
+
+
+    def reset(self):
+        self.cards = []
+        suits = ["spade", "club", "heart", "diamond"]
+        for suit in range(4):
+            ace = card(suits[suit], "a")
+            self.cards.append(ace)
+
+            for num in range(2, 11):
+                new_card = card(suits[suit], num)
+                self.cards.append(new_card)
+
+            jack = card(suits[suit], "j")
+            queen = card(suits[suit], "q")
+            king = card(suits[suit], "k")
+            self.cards.append(jack)
+            self.cards.append(queen)
+            self.cards.append(king)
