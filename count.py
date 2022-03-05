21dd78f27c4f030d580e14c52d1baf3e684403be
treverbrice <treverbrice@gmail.com>
2022-03-05 06:53:15 -0800

    added count.py

---
+++ count.py
@@ -0,0 +1,15 @@
+import hand
+
+class count(hand.hand):
+    def __init__(self, *args):
+        self.count = 0
+        self.cards = []
+        self.addCard(*args)
+
+    def addCard(self, *new_cards):
+        for card in new_cards:
+            if card.getValue <= 6:
+                self.count += 1
+            elif card.getValue >= 10:
+                self.count -= 1
+            self.cards.append(card)
